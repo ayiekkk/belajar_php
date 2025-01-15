f376394480759af428180e2285e6d71884fa95c7
ayiekkk <mohamadalief547@gmail.com>
2025-01-15 11:00:22 +0700

    add : index.php variabel

--- pt1/index.php
+++ pt1/index.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h3>
-        <?php 
-            echo "Hello World";
-        ?>
-    </h3>
-</body>
-</html>
+<?php 
+    $nama = "firman ganteng";
+
+    echo "halo nama saya adalah " . $nama;
+?>
