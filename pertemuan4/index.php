d985a3dc3e18a8a444be130660fd7fff77ce80d5
ayiekkk <mohamadalief547@gmail.com>
2025-02-12 17:06:39 +0700

    new

---
+++ pertemuan4/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <table>
+            <tr>
+                <td>baris</td>
+                <td>:</td>
+                <td>
+                    <input type="number" name="baris" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>kolom</td>
+                <td>:</td>
+                <td>
+                    <input type="number" name="cell" id="">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <button type="submit" name="submit">Run</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
